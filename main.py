1135e75f633aa0dadd7c747262a775a502c17b3f
Michael Slaughter <michaelslaughter77@gmail.com>
2025-11-28 17:59:06 -0600

    update main.py to print the count of the words in the provided path

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-path = ""
+def main():
+    path = "books/frankenstein.txt"
+    text = get_book_text(path)              #sets variable "text" to call the function "get_book_text" with input "path"
+    word_count = number_of_words(text)      #sets variable "word_count" to call the function "number_of_words" with input "text"
+    print(f"Found {word_count} total words")
+    
 
 
 def get_book_text(path):
-    with open(path) as f:
-        contents = f.read()
-    return contents
+    with open(path) as f:                   #uses "open()" to open the path to our file, then returns the text that was in the file
+        return f.read()
 
 
-
-
-
-
-
-
-
-
-
-def main():
-    print(get_book_text("books/frankenstein.txt"))
+def number_of_words(text):
+    words = text.split()                    #uses "split" to split all of the words that were read in the file in the "get_book_text" function, into a list of individual words
+    return len(words)                       #returns the length of the list as an integer
     
+
+
+
+
+
 
 main()
