7836087d0a53eee23cdb32a3a653e8dc99226d9d
Michael Slaughter <michaelslaughter77@gmail.com>
2025-11-28 17:10:35 -0600

    update main.py to read a txt file and print contents

---
+++ main.py
@@ -0,0 +1,23 @@
+path = ""
+
+
+def get_book_text(path):
+    with open(path) as f:
+        contents = f.read()
+    return contents
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+    
+
+main()
